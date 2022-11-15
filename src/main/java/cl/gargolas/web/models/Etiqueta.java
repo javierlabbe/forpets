4212701c5f312c45620da13739b70e7e058f00c5
Luis Serrano <luidserranomiranda@gmail.com>
2022-11-15 19:50:38 -0300

    Se agrega Entidad Etiqueta.java en el packcage models

---
+++ src/main/java/cl/gargolas/web/models/Etiqueta.java
@@ -0,0 +1,47 @@
+package cl.gargolas.web.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="Etiquetas")
+public class Etiqueta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idEtiqueta;
+	
+	@NotNull
+	@Size(min=0,max=200)
+	private String Descripcion;
+
+	public Etiqueta() {
+		super();
+	}
+
+	public Integer getIdEtiqueta() {
+		return idEtiqueta;
+	}
+
+	public void setIdEtiqueta(Integer idEtiqueta) {
+		this.idEtiqueta = idEtiqueta;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+		
+		
+	}
+	
+	
+
+}
