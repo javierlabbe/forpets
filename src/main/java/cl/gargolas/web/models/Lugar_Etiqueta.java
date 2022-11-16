8370a6d27d4a5e97ddcdd9b5eb8a83bacac37cd8
Luis Serrano <luidserranomiranda@gmail.com>
2022-11-16 10:15:35 -0300

    Se agrega modificacion a la Entidad Lugar_Etiqueta  se le agregan atributos  de contro de fechas

--- src/main/java/cl/gargolas/web/models/Lugar_Etiqueta.java
+++ src/main/java/cl/gargolas/web/models/Lugar_Etiqueta.java
@@ -1,8 +1,15 @@
 package cl.gargolas.web.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 @Entity 
 @Table(name="lugares_Etiquetas")
 public class Lugar_Etiqueta {
@@ -13,6 +20,13 @@ public class Lugar_Etiqueta {
 	
 	@NotNull
 	private Integer idLugar;
+	
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
 
 	//empty constructor
 	public Lugar_Etiqueta() {
@@ -20,6 +34,7 @@ public class Lugar_Etiqueta {
 	}
  
 	//Getter y Setter
+	
 	public Integer getIdEtiqueta() {
 		return idEtiqueta;
 	}
@@ -35,8 +50,34 @@ public class Lugar_Etiqueta {
 	public void setIdLugar(Integer idLugar) {
 		this.idLugar = idLugar;
 	}
-	
-	
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	//inserta la fecha del momento que se esta actualizando
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+
 
 	
 }
