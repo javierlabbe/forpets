c82622a2896ced2d4770b0429281bf843ae48000
Luis Serrano <luidserranomiranda@gmail.com>
2022-11-16 10:36:28 -0300

    Se agrega modificacion a la Entidad CategoriaReporteLugar.java  se le agregan atributos  de contro de fechas

--- src/main/java/cl/gargolas/web/models/CategoriaReporteLugar.java
+++ src/main/java/cl/gargolas/web/models/CategoriaReporteLugar.java
@@ -1,13 +1,20 @@
 package cl.gargolas.web.models;
 //importaciones
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 //Crecion de la clase o entidad
 @Entity
@@ -20,17 +27,27 @@ public class CategoriaReporteLugar {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idCategoria;
+	
 	@NotNull
 	@Size(min=0, max=201)
 	private String Descripcion;
 	
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
 	
 	//contructores
 	public CategoriaReporteLugar() {
 		super();
 	}
 
+	
+
 	// Getter y Setter 
+
 	public Integer getIdCategoria() {
 		return idCategoria;
 	}
@@ -46,7 +63,34 @@ public class CategoriaReporteLugar {
 	public void setDescripcion(String descripcion) {
 		Descripcion = descripcion;
 	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 	
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	//inserta la fecha del momento que se esta actualizando
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
 	
 
 	
