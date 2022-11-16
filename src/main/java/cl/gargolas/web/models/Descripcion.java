b02200616152a5c77a781b483e76fe4e9b488d91
ovat01 <gjulian.job@gmail.com>
2022-11-16 11:33:38 -0300

    Agrega entidad descripcion.java

--- src/main/java/cl/gargolas/web/models/Descripcion.java
+++ src/main/java/cl/gargolas/web/models/Descripcion.java
@@ -1,25 +1,39 @@
 package cl.gargolas.web.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name = "descripcion")
 public class Descripcion {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idDescripcion;
-	
+
 	@NotNull
 	@Size(min = 1, max = 30)
 	private String descripcion;
-	
+
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+
 	public Descripcion() {
 		super();
 	}
@@ -40,6 +54,14 @@ public class Descripcion {
 		this.descripcion = descripcion;
 	}
 	
-	
-	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+
 }
