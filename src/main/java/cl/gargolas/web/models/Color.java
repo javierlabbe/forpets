45645739402ced4c05c3354e25b6d9dac42dc7f6
franmarvelous <franmarvelous@gmail.com>
2022-11-16 10:28:45 -0300

    Entidad Color actualizada

--- src/main/java/cl/gargolas/web/models/Color.java
+++ src/main/java/cl/gargolas/web/models/Color.java
@@ -1,34 +1,92 @@
 package cl.gargolas.web.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
-@Table(name="colores")
+@Table(name = "colores")
 public class Color {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idColor;
 	private String descripcion;
+	@Column(updatable = false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
 	
 	public Color() {
 		super();
 	}
+	
 
 	public Integer getIdColor() {
 		return idColor;
 	}
 
+
+
 	public void setIdColor(Integer idColor) {
 		this.idColor = idColor;
 	}
+
+
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	
 
+
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
+	@PrePersist
+	protected void onCreate(){
+		this.createdAt = new Date();
+		}
+
+	@PreUpdate
+		protected void onUpdate(){
+		this.updatedAt = new Date();
+		}
 }
+
