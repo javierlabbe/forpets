0157a96655648e39fd3acacc8017a3e4abfc4050
franmarvelous <franmarvelous@gmail.com>
2022-11-16 10:35:21 -0300

    Entidad Usuario actualizado

--- src/main/java/cl/gargolas/web/models/Usuario.java
+++ src/main/java/cl/gargolas/web/models/Usuario.java
@@ -1,52 +1,187 @@
 package cl.gargolas.web.models;
 
+import java.sql.Blob;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="usuarios")
 public class Usuario {
-	
-	private Long id;  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsuario;  
 	private String nombre;
-	private String correo;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private Date fechaNacimiento;
+	private String rut;
+	private String numDireccion;
+	private String telefono;
+	private String email;
 	private String password;
 	private String password2; // para confirmar contraseña
-	private String nick;
-	// constructores
+	private Blob imagen;
+	private Integer idDireccion;
+	private Integer idDescripcion;
+	@Column(updatable = false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	//Constructor vacío
 	public Usuario() {
 		super();
 	}
-	// getters and setters
-	public Long getId() {
-		return id;
+	
+	//Getters y setters
+	public Long getIdUsuario() {
+		return idUsuario;
 	}
-	public void setId(Long id) {
-		this.id = id;
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getCorreo() {
-		return correo;
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
 	}
-	public void setCorreo(String correo) {
-		this.correo = correo;
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
 	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getRut() {
+		return rut;
+	}
+
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+
+	public String getNumDireccion() {
+		return numDireccion;
+	}
+
+	public void setNumDireccion(String numDireccion) {
+		this.numDireccion = numDireccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getPassword2() {
 		return password2;
 	}
+
 	public void setPassword2(String password2) {
 		this.password2 = password2;
 	}
-	public String getNick() {
-		return nick;
+
+	public Blob getImagen() {
+		return imagen;
 	}
-	public void setNick(String nick) {
-		this.nick = nick;
+
+	public void setImagen(Blob imagen) {
+		this.imagen = imagen;
 	}
+
+	public Integer getIdDireccion() {
+		return idDireccion;
+	}
+
+	public void setIdDireccion(Integer idDireccion) {
+		this.idDireccion = idDireccion;
+	}
+
+	public Integer getIdDescripcion() {
+		return idDescripcion;
+	}
+
+	public void setIdDescripcion(Integer idDescripcion) {
+		this.idDescripcion = idDescripcion;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@PrePersist
+	protected void onCreate(){
+		this.createdAt = new Date();
+		}
+
+	@PreUpdate
+		protected void onUpdate(){
+		this.updatedAt = new Date();
+		}
+	
+
 }
