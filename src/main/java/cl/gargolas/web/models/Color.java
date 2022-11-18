188ffad6349e40ab14a53ce3667ae64a30fdef1d
javierlabbe <javier.labbe@uc.cl>
2022-11-17 22:29:09 -0300

    se añade relacion oneToMany

--- src/main/java/cl/gargolas/web/models/Color.java
+++ src/main/java/cl/gargolas/web/models/Color.java
@@ -2,11 +2,14 @@ package cl.gargolas.web.models;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -19,7 +22,12 @@ public class Color {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idColor;
+	
 	private String descripcion;
+	
+	@OneToMany(mappedBy = "color", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private PerfilMascota perfilMascota;
+	
 	@Column(updatable = false)
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createdAt;
@@ -30,54 +38,45 @@ public class Color {
 		super();
 	}
 	
-
 	public Integer getIdColor() {
 		return idColor;
 	}
-
-
 
 	public void setIdColor(Integer idColor) {
 		this.idColor = idColor;
 	}
 
-
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
-
 
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	public PerfilMascota getPerfilMascota() {
+		return perfilMascota;
+	}
 
+	public void setPerfilMascota(PerfilMascota perfilMascota) {
+		this.perfilMascota = perfilMascota;
+	}
 
 	public Date getCreatedAt() {
 		return createdAt;
 	}
 
-
-
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-
-
 
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
 
-
-
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-
-
 
 	@PrePersist
 	protected void onCreate(){
