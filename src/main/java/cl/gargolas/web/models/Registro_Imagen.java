74885a693431f453631bff54aa8e0a76b5cee0eb
franmarvelous <franmarvelous@gmail.com>
2022-11-17 23:12:26 -0300

    Se agregan Foreign Keys a archivo RegistroImagen

--- src/main/java/cl/gargolas/web/models/Registro_Imagen.java
+++ src/main/java/cl/gargolas/web/models/Registro_Imagen.java
@@ -4,70 +4,77 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mysql.cj.jdbc.Blob;
 
 @Entity
-@Table(name="registrosImagenes")
-public class RegistroImagen {
+@Table(name="registros_imagenes")
+public class Registro_Imagen {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idImagen;
+	//Agregando imagen como FK, relacion manytoOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@Column(name="imagen_id")
+	private Imagen imagen;
 	
-	private Blob imagen;
+	//Agregando reporte como FK, relacion ManyToOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@Column(name="reporte_id")
+	private Reporte reporte;
+	
+	//Agregando lugarInteres como FK, relacion ManyToOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@Column(name="lugarInteres_id")
+	private LugarInteres lugarInteres;
+	
 	
 	@Column(updatable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createdAt;
-
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date updatedAt;
 
-	public RegistroImagen() {
-		super();
-	}
-
-	public Integer getIdImagen() {
-		return idImagen;
-	}
-
-	public void setIdImagen(Integer idImagen) {
-		this.idImagen = idImagen;
-	}
-
-	public Blob getImagen() {
+	
+	public Imagen getImagen() {
 		return imagen;
 	}
 
-	public void setImagen(Blob imagen) {
+	public void setImagen(Imagen imagen) {
 		this.imagen = imagen;
 	}
-	
-	public Date getCreatedAt() {
-		return createdAt;
+
+	public Reporte getReporte() {
+		return reporte;
 	}
 
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
+	public void setReporte(Reporte reporte) {
+		this.reporte = reporte;
 	}
 	
+	public LugarInteres getLugarInteres() {
+		return lugarInteres;
+	}
+
+	public void setLugarInteres(LugarInteres lugarInteres) {
+		this.lugarInteres = lugarInteres;
+	}
+
 	@PrePersist
 	protected void onCreate() {
 		this.createdAt = new Date();
