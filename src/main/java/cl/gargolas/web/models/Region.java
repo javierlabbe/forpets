ec4c971ef2a2787d91b08cf137cb11fc98dfb200
RocioQ01 <rocioquezadarodriguez01@gmail.com>
2022-11-18 23:57:19 -0300

    Relaciones entre entidades

--- src/main/java/cl/gargolas/web/models/Region.java
+++ src/main/java/cl/gargolas/web/models/Region.java
@@ -1,12 +1,16 @@
 package cl.gargolas.web.models;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -14,6 +18,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name="regiones")
@@ -33,6 +40,11 @@ public class Region {
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date updatedAt;
+	
+	//Relacion OneToMany de region a ciudad
+	@JsonIgnore
+	@OneToMany(mappedBy = "region",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Ciudad> ciudad;
 	
 	public Region() {
 		super();
