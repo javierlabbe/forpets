3b0f49ed5ed8cd28fd91d4a5fd2d02563be7d029
Luis Serrano <luidserranomiranda@gmail.com>
2022-11-21 13:38:45 -0300

    Se agrega lombok

--- src/main/java/cl/gargolas/web/models/Ubicacion.java
+++ src/main/java/cl/gargolas/web/models/Ubicacion.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,13 +13,27 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 //testing
+@Entity
+@Table(name="ubicaciones")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Ubicacion {
 	
 	@Id
@@ -41,53 +56,7 @@ public class Ubicacion {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;
 
-	public Ubicacion() {
-		super();
-	}
-
-	public Integer getIdUbicacion() {
-		return idUbicacion;
-	}
-
-	public void setIdUbicacion(Integer idUbicacion) {
-		this.idUbicacion = idUbicacion;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
 	
-	
-
-	public List<Usuario> getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(List<Usuario> usuario) {
-		this.usuario = usuario;
-	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
 	@PrePersist
 	protected void onCreate() {
 		this.createdAt = new Date();
