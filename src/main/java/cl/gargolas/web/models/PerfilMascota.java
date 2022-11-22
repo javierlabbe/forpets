93eda39878b2ea1a3c5342695c24cb630eb115ba
javierlabbe <javier.labbe@uc.cl>
2022-11-22 11:00:32 -0300

    se modifican atributos de PerfilMascosta.java

--- src/main/java/cl/gargolas/web/models/PerfilMascota.java
+++ src/main/java/cl/gargolas/web/models/PerfilMascota.java
@@ -1,213 +1,113 @@
 package cl.gargolas.web.models;
-
+ 
 import java.sql.Blob;
 import java.util.Date;
-
+ 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+ 
 import org.springframework.format.annotation.DateTimeFormat;
+ 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+ 
 @Entity
 @Table(name="mascotas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class PerfilMascota {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idPerfilMascota;
-	
+ 
 	@NotNull
 	@Size(min=1,max=15, message= "Error en el ingreso del nombre")
 	private String nombre;
-	
+ 
 	private Integer nChip;
-	
-	@NotNull
-	@Size(min=1,max=15) // revisar colores mas largos y cortos
-	private String colorPelaje;
-	
-	@NotNull
-	@Size(min=1,max=15) //revisar patrones
-	private String patron;
-	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idRaza;
-	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idEspecie;
-	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idPatron;
-	
+ 
 	private Blob imagen; //importo java.sql
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "raza_id")
+	private Raza raza;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "especie_id")
+	private Especie especie;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "patron_id")
+	private Patron patron;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "color_id")
+	private Color color;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "tamanio_id")
+	private Tamanio tamanio;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "descripcion_id")
+	private Descripcion descripcion;
+ 
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
 	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idColor;
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "rangoEtario_id")
+	private RangoEtario rangoEtario;
 	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idTamano;
-	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idDescripcion;
-	
-	@NotNull
-	@Size(min=1,max=15)
-	private Long idUsuario;
-	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "sexo_id")
+	private Sexo sexo;
+ 
 	@Column(updatable = false)
 	@DateTimeFormat(pattern="yyyy-MM-dd") 
 	private Date createdAt;
-	
+ 
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;
-
-	//Constructor
-	public PerfilMascota() {
-		super();
-	}
-	//Getters and setters
-
-	public Long getIdPerfilMascota() {
-		return idPerfilMascota;
-	}
-
-	public void setIdPerfilMascota(Long idPerfilMascota) {
-		this.idPerfilMascota = idPerfilMascota;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Integer getnChip() {
-		return nChip;
-	}
-
-	public void setnChip(Integer nChip) {
-		this.nChip = nChip;
-	}
-
-	public String getColorPelaje() {
-		return colorPelaje;
-	}
-
-	public void setColorPelaje(String colorPelaje) {
-		this.colorPelaje = colorPelaje;
-	}
-
-	public String getPatron() {
-		return patron;
-	}
-
-	public void setPatron(String patron) {
-		this.patron = patron;
-	}
-
-	public Long getIdRaza() {
-		return idRaza;
-	}
-
-	public void setIdRaza(Long idRaza) {
-		this.idRaza = idRaza;
-	}
-
-	public Long getIdEspecie() {
-		return idEspecie;
-	}
-
-	public void setIdEspecie(Long idEspecie) {
-		this.idEspecie = idEspecie;
-	}
-
-	public Long getIdPatron() {
-		return idPatron;
-	}
-
-	public void setIdPatron(Long idPatron) {
-		this.idPatron = idPatron;
-	}
-
-	public Blob getImagen() {
-		return imagen;
-	}
-
-	public void setImagen(Blob imagen) {
-		this.imagen = imagen;
-	}
-
-	public Long getIdColor() {
-		return idColor;
-	}
-
-	public void setIdColor(Long idColor) {
-		this.idColor = idColor;
-	}
-
-	public Long getIdTamano() {
-		return idTamano;
-	}
-
-	public void setIdTamano(Long idTamano) {
-		this.idTamano = idTamano;
-	}
-
-	public Long getIdDescripcion() {
-		return idDescripcion;
-	}
-
-	public void setIdDescripcion(Long idDescripcion) {
-		this.idDescripcion = idDescripcion;
-	}
-
-	public Long getIdUsuario() {
-		return idUsuario;
-	}
-
-	public void setIdUsuario(Long idUsuario) {
-		this.idUsuario = idUsuario;
-	}
-	
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) { 
-		this.createdAt = createdAt;
-	}
-	
+ 
 	@PrePersist
 	protected void onCreate(){
 		this.setUpdatedAt(new Date());
 	}
-	
+  
 	@PreUpdate
 	protected void onUpdate(){
 		this.setUpdatedAt(new Date());
 	}
-	
+ 
 }
+ 
