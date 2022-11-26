1c9bc10b0a2fdcce2a4bcc0b9e0455b8ea00a9a3
Luis Serrano <luidserranomiranda@gmail.com>
2022-11-25 22:05:43 -0300

    Corrige error de entidad color

--- src/main/java/cl/gargolas/web/models/Color.java
+++ src/main/java/cl/gargolas/web/models/Color.java
@@ -1,6 +1,8 @@
 package cl.gargolas.web.models;
 
 import java.util.Date;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,6 +16,8 @@ import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,8 +39,9 @@ public class Color {
 	
 	private String descripcion;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "color", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private PerfilMascota perfilMascota;
+	private List<PerfilMascota> perfilMascota;
 	
 	@Column(updatable = false)
 	@DateTimeFormat(pattern="yyyy-MM-dd")
