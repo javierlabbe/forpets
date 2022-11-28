0e315f64948add505e2c1cd96e6367be56240cf1
javierlabbe <javier.labbe@uc.cl>
2022-11-28 12:01:14 -0300

    se añade id a RegistroValoracion y se corrigen relaciones manyToMany a manyToOne

---
+++ src/main/java/cl/gargolas/web/models/RegistroValoracion.java
@@ -0,0 +1,47 @@
+package cl.gargolas.web.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="valoraciones")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RegistroValoracion {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idRegistroValoracion;
+	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="valoracion_id")
+	private Valoracion valoracion;
+
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="lugarInteres_id")
+	private LugarInteres lugarInteres;
+	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="usuario_id")
+	private Usuario usuario;
+}
