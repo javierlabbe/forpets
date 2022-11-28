72cef5d40f692fc6280e839f2ebd1bce43d21f76
javierlabbe <javier.labbe@uc.cl>
2022-11-28 11:37:58 -0300

    se agrega entidad Registro_Imagen

---
+++ src/main/java/cl/gargolas/web/models/Registro_Imagen.java
@@ -0,0 +1,76 @@
+package cl.gargolas.web.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="registros_imagenes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Registro_Imagen {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idRegistroImagen;
+	
+	//Agregando imagen como FK, relacion manytoOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="imagen_id")
+	private Imagen imagen;
+	
+	//Agregando reporte como FK, relacion ManyToOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="reporte_id")
+	private Reporte reporte;
+	
+	//Agregando lugarInteres como FK, relacion ManyToOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="lugarInteres_id")
+	private LugarInteres lugarInteres;
+	
+	
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+
+	@PreUpdate
+	//inserta la fecha del momento que se esta actualizando
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+		
+}
