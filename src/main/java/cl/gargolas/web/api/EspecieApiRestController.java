09aa7bc9fd2d78a21960eb1cc1b1d9a4771d438b
unknown <luidserranomiranda@gmail.com>
2023-01-04 17:39:52 -0300

    Se agrega api para combobox de especie y raza

--- src/main/java/cl/gargolas/web/api/EspecieApiRestController.java
+++ src/main/java/cl/gargolas/web/api/EspecieApiRestController.java
@@ -1,17 +1,29 @@
 package cl.gargolas.web.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cl.gargolas.web.models.Comuna;
 import cl.gargolas.web.models.Especie;
+import cl.gargolas.web.models.Raza;
 import cl.gargolas.web.services.EspecieServiceImpl;
+import cl.gargolas.web.services.RazaServiceImpl;
 
 @RestController
 @RequestMapping("/apiEspecie")
 public class EspecieApiRestController {
+	
+	@Autowired
+	RazaServiceImpl razaServiceImpl;
+	
 	@Autowired
 	EspecieServiceImpl especieServiceImpl;
 	
@@ -38,4 +50,13 @@ public class EspecieApiRestController {
 		Especie  especie = especieServiceImpl.obtenerEspecie(id);
 		return especie.toString();
 	}
+	
+	
+	  @RequestMapping(value = "/obtenerRaza", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE) 
+	  public @ResponseBody List<Raza>
+	  findAllByRazas(@RequestParam(value="id",required = false) Long id) {
+		 return especieServiceImpl.listaRazas(id);
+		 }
+	  
+	 
 }
